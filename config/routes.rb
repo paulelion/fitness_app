aa1608272c8948f9d04c3a686fe1444527c37ccd
Patrice Alleyne <muniner1@gmail.com>
2017-09-08 10:14:09 -0400

    Exercises and routes
    arms, leg, shoulder, abs and cardio was created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'routines#index'
+  get '/muscle_groups', to: 'muscle_groups#index'
+  get '/arm', to: 'muscle_groups#arm'
 
   resources :routines, only: [] do 
   	resources :schedules, only: [:index, :show] do 
