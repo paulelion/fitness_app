a02e0028fb10f2ff6b83466ef68360f12de539d8
Patrice Alleyne <muniner1@gmail.com>
2017-09-08 10:58:48 -0400

    created the routes and muscle group

--- app/controllers/muscle_groups_controller.rb
+++ app/controllers/muscle_groups_controller.rb
@@ -2,12 +2,7 @@ class MuscleGroupsController < ApplicationController
 
 	def index
 		@muscle_group
-	end
-
-	def show
-		@muscle_group = Muscle_group.find_by(params[:id])
-	end
-
+    end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
   get '/', to: 'routines#index'
   get '/muscle_groups', to: 'muscle_groups#index'
   get '/arm', to: 'muscle_groups#arm'
-
-  
+  get '/shoulder', to: 'muscle_groups#shoulder'
+  get '/chest', to: 'muscle_groups#chest'
+  get '/abs', to: 'muscle_groups#abs'
+  get '/back', to: 'muscle_groups#back'
+  get '/leg', to: 'muscle_groups#leg'
+  get '/cardio', to: 'muscle_groups#cardio'  
 end
 
