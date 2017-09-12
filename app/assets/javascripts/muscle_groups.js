8bee245f07ff52e9bae1fe651fcf47ddf241e075
Paul Elion <paul.j.elion@gmail.com>
2017-09-12 14:20:02 -0400

    add muscle_group functionality

---
+++ app/assets/javascripts/muscle_groups.js
@@ -0,0 +1,12 @@
+
+		function handleMouseOver(id) {
+			let li = document.getElementById(id);
+			li.classList.add('active')
+		}
+
+		function handleMouseOut(id) {
+			let li = document.getElementById(id);
+			li.classList.remove('active')
+		}
+
+
