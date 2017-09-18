b001cf69b6e1e5e37f1d4e8610d9475b56c9104a
Paul Elion <paul.j.elion@gmail.com>
2017-09-18 10:17:21 -0400

    fix functionality for touchpoints on muscle man image

--- app/assets/javascripts/muscle_groups.js
+++ app/assets/javascripts/muscle_groups.js
@@ -1,15 +1,17 @@
 
 function handleMouseOver(className) {
+	console.log("mouse over")
 	let divs = document.querySelectorAll(className);
 	for (i = 0; i < divs.length; i++) { 
-	divs.classList.add('active')
+	  divs[i].classList.add('active')
 	}
 }
 
 function handleMouseOut(className) {
+	console.log("mouse out")
 	let divs = document.querySelectorAll(className);
 	for (i = 0; i < divs.length; i++) { 
-	divs.classList.remove('active')
+	  divs[i].classList.remove('active')
 	}
 }
 
