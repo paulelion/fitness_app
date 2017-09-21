9502045eec7c4a943c5929340d544532cf883e57
Patrice Alleyne <muniner1@gmail.com>
2017-09-21 16:50:48 -0400

    update config

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module FitnessApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.initialize_on_precompile = false
 
+   end
   end
 end
